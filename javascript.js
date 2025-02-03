9015ca6625f4920f6f36f06f986c666604e62551
loops <128599880+loopsc@users.noreply.github.com>
2025-02-03 15:43:58 +1300

    Grid size adjustable
    User can click button and set the grid size.
    New Grid is undrawable however.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,50 @@
-const container = document.querySelector('#container')
+const container = document.querySelector("#container");
+const inputGridButton = document.querySelector(".squares-input");
+const startingGrid = 16;
+const containerSize = 960;
 
-for (let i=0; i < 256; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
+let cellDimensions = containerSize/startingGrid
 
-    container.appendChild(square)
+let gridList = document.querySelectorAll(".cell");
+
+drawGrid(startingGrid);
+
+
+gridList.forEach((grid) => {
+  grid.addEventListener("mouseover", () => {
+    grid.style.backgroundColor = "grey";
+  });
+});
+
+inputGridButton.addEventListener("click", () => {
+  userInputGrids = prompt("Number of squares per side?");
+  while (userInputGrids > 100) {
+    userInputGrids = prompt("Number of squares per side? (<= 100)");
+  }
+
+  removeGrid();
+  
+  cellDimensions = containerSize / userInputGrids
+
+  drawGrid(userInputGrids);
+});
+
+function removeGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
 }
 
+function drawGrid(numGrid) {
+  for(let row = 0; row < numGrid; row++){
+    for(let col = 0; col < numGrid; col++){
+      const cell = document.createElement('div')
+      cell.classList.add('cell')
+      
+      cell.setAttribute('style', `width: ${cellDimensions}px; height: ${cellDimensions}px; `)
 
-gridList = document.querySelectorAll('.square')
-
-gridList.forEach(grid => {
-    grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = 'grey'
-    })
-});
+      container.appendChild(cell)
+    }
+  }
+  gridList = document.querySelectorAll(".cell");
+}
