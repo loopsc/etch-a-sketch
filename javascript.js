fd8784304620373a3019d87203280af1e100128a
loops <128599880+loopsc@users.noreply.github.com>
2025-02-05 01:19:42 +1300

    Cells darken on repeated hover
    Added functionality to allow cells to reduce in opactity every time it is hovered, up to a total of 10 times, which completely darkens cell.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const container = document.querySelector("#container");
 const inputGridButton = document.querySelector(".squares-input");
 const startingGrid = 16;
 const containerSize = 500;
-let cellDimensions = containerSize/startingGrid
+let cellDimensions = containerSize / startingGrid;
 let gridList = document.querySelectorAll(".cell");
 
 drawGrid(startingGrid);
@@ -15,7 +15,7 @@ inputGridButton.addEventListener("click", () => {
 
   removeGrid();
 
-  cellDimensions = containerSize / userInputGrids
+  cellDimensions = containerSize / userInputGrids;
 
   drawGrid(userInputGrids);
 });
@@ -27,21 +27,29 @@ function removeGrid() {
 }
 
 function drawGrid(numGrid) {
-  for(let row = 0; row < numGrid; row++){
-    for(let col = 0; col < numGrid; col++){
-      const cell = document.createElement('div')
-      cell.classList.add('cell')
-      
-      cell.setAttribute('style', `width: ${cellDimensions}px; height: ${cellDimensions}px; `)
+  for (let row = 0; row < numGrid; row++) {
+    for (let col = 0; col < numGrid; col++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
 
-      container.appendChild(cell)
+      cell.setAttribute(
+        "style",
+        `width: ${cellDimensions}px; height: ${cellDimensions}px; opacity: 1`
+      );
+
+      container.appendChild(cell);
     }
   }
   gridList = document.querySelectorAll(".cell");
 
   gridList.forEach((grid) => {
     grid.addEventListener("mouseover", () => {
-      grid.style.backgroundColor = getRandomRGB();
+      grid.style.backgroundColor = 'grey';
+      // grid.style.backgroundColor = getRandomRGB();
+
+      // Changes opacity of cell on every hover
+      let currentOpacity = +grid.style.getPropertyValue('opacity')
+      grid.style.opacity = currentOpacity <= 1 ? currentOpacity - 0.1: currentOpacity;
     });
   });
 }
@@ -50,5 +58,5 @@ function getRandomRGB() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
-  return `rgb(${r},${g},${b})`
+  return `rgb(${r},${g},${b})`;
 }
